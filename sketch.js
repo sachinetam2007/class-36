1c34727ff61c44af63b7cf7ceae0745c8d1e98be
Sachi Netam <71205766+sachinetam2007@users.noreply.github.com>
2020-12-19 12:00:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database,playerCount;
+var gameState=0;
+var form,player,game;
+var allPlayers;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+  game=new Game();
+  game.getState()
+  game.start()
+  
+}
+
+function draw(){
+ if(playerCount===4){
+   game.update(1)
+
+ }
+ if (gameState===1){
+   clear ()
+   game.play()
+ }
+}
+
